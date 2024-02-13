d4a6d3f51eaa30f756aa96878f59d3e9b4f9dfbf
rmalcac <rmalcac16_1@unc.edu.pe>
2024-02-13 10:12:31 -0500

    NoMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,9 +72,9 @@ $app->configure('app');
 |
 */
 
-$app->middleware([
-    App\Http\Middleware\EncryptResponse::class
-]);
+// $app->middleware([
+//     App\Http\Middleware\EncryptResponse::class
+// ]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
